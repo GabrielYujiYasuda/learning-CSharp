e6cce6abbc43466097e002016cef38fceb6dc262
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-04-29 23:15:14 -0300

    Initial commit

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string closingMessage = "Press enter to close!";
+        int age = 32;
+
+        Console.WriteLine(age);
+
+        Console.WriteLine(closingMessage);
+        Console.ReadLine();
+    }
+}
