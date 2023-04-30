7fc77541068334058be7bd2b3964a8cb9d74e867
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-04-29 21:09:07 -0300

    Floating variables

---
+++ FloatingVariables/FloatingVariables/Program.cs
@@ -0,0 +1,15 @@
+﻿double num;
+num = 22.9;
+
+string closingMessage = "Press enter to leave...";
+
+Console.WriteLine("Minha idade é: " + num);
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("The actual index is: " + i);
+}
+
+Console.WriteLine("Test");
+Console.WriteLine(closingMessage);
+Console.ReadLine();
