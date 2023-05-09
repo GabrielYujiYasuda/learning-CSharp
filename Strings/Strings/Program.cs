e89918bfe28dc3ac96242b24f2279be21081d281
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-05-08 18:35:54 -0600

    Fixing name

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,7 @@
+﻿string firstPhrase = "My name is ";
+string name = "";
+
+string languages = "Avaiable courses: \n- Ruby \n- Java \n- C# \n- TypeScript \n- PHP"; 
+
+Console.WriteLine(firstPhrase + name);
+Console.WriteLine(languages);
