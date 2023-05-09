95196ff4050def18ec4df70cb4ec7f56b5842e0d
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-05-08 18:44:08 -0600

    Conditionals if

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,16 @@
+﻿double age;
+
+Console.WriteLine("What is your age?");
+//Need to convert the input to integer.
+age = Convert.ToInt32(Console.ReadLine());
+
+if (age >= 21)
+{
+    Console.WriteLine("Fine!!!");
+} else if (age < 21 && age >= 0)
+{
+    Console.WriteLine("You cannot enter!");
+} else
+{
+    Console.WriteLine("Some kind of error");
+}
