d158bffd42a6c1be7244b64d31fef8a91027333c
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-05-08 18:19:15 -0600

    Chars

---
+++ Chars/Chars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char letra = 'A';
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+    }
+}
+
+
+
