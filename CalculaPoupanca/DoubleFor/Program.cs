f1f1b070cef9eb45d5b192ceeb9e16c509468069
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-05-17 15:11:10 -0300

    Double for

---
+++ CalculaPoupanca/DoubleFor/Program.cs
@@ -0,0 +1,12 @@
+﻿for (int countRows = 0; countRows < 10; countRows++)
+{
+    for (int countColumns = 0; countColumns < 10; countColumns++)
+    {
+        Console.Write("*");
+
+        if (countColumns >= countRows)
+            break;
+    }
+
+    Console.WriteLine();
+}
